83b8399c70282388f7e6897f334bd2cd385372a3
Tarun <tk7864225@gmail.com>
2025-09-13 11:07:06 +0530

    Update community-feed.tsx

--- components/community-feed.tsx
+++ components/community-feed.tsx
@@ -165,7 +165,7 @@ export default function CommunityFeed() {
   }, [])
 
   const handleLike = (postId: string) => {
-    if (!user) return
+    //if (!user) return
 
     setPosts((prev) =>
       prev.map((post) => {
